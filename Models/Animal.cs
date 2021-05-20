e1013950a78bf50c567f4be758a48b3c8790706d
Patryk Osak <pionchaoo@gmail.com>
2021-05-20 16:15:33 +0200

    Add models

---
+++ Models/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektSchronisko.Models
+{
+    public class Animal
+    {
+        public Guid IdAnimal { get; set; }
+        public string Name { get; set; }
+        public TypeAnimal TypeAnimale { get; set; }
+        public Age AgeAnimal { get; set; }
+        public Guid AdderId { get; set; }
+        public Race RaceAnimal { get; set; }
+    }
+}
---
+++ Models/Enums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektSchronisko.Models
+{
+    public enum TypeAnimal
+    {
+        Dog,
+        Cat,
+        Other
+    }
+    public enum Age
+    {
+       Months13,
+       Months36,
+       Months612,
+       Year,
+       Years2,
+       Years3,
+       Older
+
+    }
+    public enum Race
+    {
+        Dachshund,
+        GermanShepherd,
+        Other
+    }
+}
