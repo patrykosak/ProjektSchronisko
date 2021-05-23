271e8c1e41b3da772a5f8cb6fb0b394baed9b34d
Rafał Zaborowski <raffik2000@gmail.com>
2021-05-23 20:51:27 +0200

    Adding possibility of reporting, deleting, editing, viewing notifications

--- Pages/Reports/Create.cshtml.cs
+++ Pages/Reports/Create.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,21 +8,22 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using ProjektSchronisko.AppData;
 using ProjektSchronisko.Models;
 
-namespace ProjektSchronisko.Pages.Animals
+namespace ProjektSchronisko.Pages.Reports
 {
     public class CreateModel : PageModel
     {
-        private readonly ProjektSchronisko.AppData.AnimalsContext _context;
+        private readonly AnimalsContext _context;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly IHostingEnvironment _IHostingEnvironment;
 
-        public CreateModel(ProjektSchronisko.AppData.AnimalsContext context, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, IWebHostEnvironment webHostEnvironment, IHostingEnvironment IHostingEnvironment)
+        public CreateModel(AnimalsContext context, SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager, IWebHostEnvironment webHostEnvironment,
+            IHostingEnvironment IHostingEnvironment)
         {
             _context = context;
             _signInManager = signInManager;
@@ -30,7 +31,7 @@ namespace ProjektSchronisko.Pages.Animals
             _webHostEnvironment = webHostEnvironment;
             _IHostingEnvironment = IHostingEnvironment;
         }
-
+       
         [BindProperty]
         public IFormFile Photo { get; set; }
 
@@ -40,36 +41,33 @@ namespace ProjektSchronisko.Pages.Animals
         }
 
         [BindProperty]
-        public Animal Animal { get; set; }
-
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public ReportAnimal ReportAnimal { get; set; }
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
-            {
                 return Page();
-            }
+            
             if (_signInManager.IsSignedIn(User))
-                Animal.AdderId = _userManager.GetUserId(User);
-            Animal.AddDate = DateTime.Now;
+                ReportAnimal.AdderId = _userManager.GetUserId(User);
+            ReportAnimal.AddDate = DateTime.Now;
+
             if (Photo != null)
             {
                 var FileUpload = Path.Combine(_IHostingEnvironment.WebRootPath, "Images", Photo.FileName);
                 using (var Fs = new FileStream(FileUpload, FileMode.Create))
                 {
-                    Animal.PhotoPath = Photo.FileName;
+                    ReportAnimal.PhotoPath = Photo.FileName;
                     await Photo.CopyToAsync(Fs);
                 }
-                _context.Animals.Add(Animal);
+                _context.ReportAnimal.Add(ReportAnimal);
             }
-            
             else
             {
-                _context.Animals.Add(Animal);
+                _context.ReportAnimal.Add(ReportAnimal);
             }
             await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("./ReportAboutLossOfPet");
         }
     }
 }
