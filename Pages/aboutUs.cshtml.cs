e84c141f3f7109c1e45582ce6369c8d40dc20ec7
Patryk Osak <pionchaoo@gmail.com>
2021-05-27 15:07:16 +0200

    Add aboutUs page

---
+++ Pages/aboutUs.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjektSchronisko.Pages
+{
+    public class aboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
